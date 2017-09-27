519bbda324f0c1707259a5674c52be5a56414269
Eason <easonchennz@gmail.com>
2017-09-27 20:32:49 +1300

    add mirasvit search

---
+++ app/code/local/Mirasvit/SearchSphinx/Helper/Code.php
@@ -0,0 +1,10 @@
+<?php
+class Mirasvit_SearchSphinx_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "8TMKR6P4YQ";
+    protected $s = "SSU";
+    protected $l = "15577";
+    protected $v = "2.3.4";
+    protected $b = "1379";
+    protected $d = "photogear.co.nz";
+}
